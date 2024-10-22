226e8251a0e80958db1e1aff03d1640b064c6187
Alesha <121282755+oAleshao@users.noreply.github.com>
2024-10-22 20:43:42 +0300

    Initial commit

---
+++ app/src/main/java/itstep/learning/demonandandroid/MainActivity.java
@@ -0,0 +1,31 @@
+package itstep.learning.demonandandroid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        findViewById(R.id.main_btn_calc).setOnClickListener(this::onCalcButtonClick);
+    }
+
+    private void onCalcButtonClick(View view){
+        Toast.makeText(this, "Calculator will be here", Toast.LENGTH_SHORT).show();
+    }
+}
